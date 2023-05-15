47d8e3a3179e58754c56507a8226c4d64201efd7
Dennis Soemers <d.soemers@gmail.com>
2023-05-15 20:24:35 +0200

    fixed main

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -1,12 +1,64 @@
 #include "hooks.h"
 #include "dataHandler.h"
+#include <stddef.h>
 
-void Load()
-{
-	dataHandler::readSettings();
-	if (settings::noRegenOnBlock) {
-		logger::info("no regen on block, installing hook!");
-		staminaRegenHook::InstallHook();
-	}
-	hitEventHook::InstallHook();
+using namespace SKSE;
+using namespace SKSE::log;
+using namespace SKSE::stl;
+
+namespace {
+    /**
+     * Setup logging.
+     */
+    void InitializeLogging() {
+        auto path = log_directory();
+        if (!path) {
+            report_and_fail("Unable to lookup SKSE logs directory.");
+        }
+        *path /= PluginDeclaration::GetSingleton()->GetName();
+        *path += L".log";
+
+        std::shared_ptr<spdlog::logger> log;
+        if (IsDebuggerPresent()) {
+            log = std::make_shared<spdlog::logger>("Global", std::make_shared<spdlog::sinks::msvc_sink_mt>());
+        } else {
+            log = std::make_shared<spdlog::logger>(
+                "Global", std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true));
+        }
+
+#ifndef NDEBUG
+        const auto level = spdlog::level::trace;
+#else
+        const auto level = spdlog::level::info;
+#endif
+
+        log->set_level(level);
+        log->flush_on(level);
+
+        spdlog::set_default_logger(std::move(log));
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [%t] [%s:%#] %v");
+    }
+}  // namespace
+
+/**
+ * This is the main callback for initializing the SKSE plugin, called just before Skyrim runs its main function.
+ */
+SKSEPluginLoad(const LoadInterface* skse) {
+    InitializeLogging();
+
+    auto* plugin = PluginDeclaration::GetSingleton();
+    auto version = plugin->GetVersion();
+    log::info("{} {} is loading...", plugin->GetName(), version);
+
+    Init(skse);
+
+    dataHandler::readSettings();
+    if (settings::noRegenOnBlock) {
+        logger::info("no regen on block, installing hook!");
+        staminaRegenHook::InstallHook();
+    }
+    hitEventHook::InstallHook();
+
+    log::info("{} has finished loading.", plugin->GetName());
+    return true;
 }
